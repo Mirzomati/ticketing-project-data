f6239d35295dfaff182d020b19319767d2a227ae
Muzaffarzoda <muzaffarzoda@gmail.com>
2023-06-04 17:02:42 -0400

    Day 01 project

--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -1,13 +1,17 @@
 package com.cydeo.entity;
 
 import com.cydeo.enums.Gender;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User extends BaseEntity {
 
     private String firstName;
@@ -16,7 +20,10 @@ public class User extends BaseEntity {
     private String passWord;
     private boolean enabled;
     private String phone;
+
+    @ManyToOne
     private Role role;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
     public User(Long id, LocalDateTime insertDateTime, Long insertUserId, LocalDateTime lastUpdateDateTime, Long lastUpdateUserId, String firstName, String lastName, String userName, String passWord, boolean enabled, String phone, Role role, Gender gender) {
