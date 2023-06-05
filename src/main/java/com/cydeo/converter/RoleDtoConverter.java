cf78b3204246684ef41e24d9011fa3aff20f6a97
Muzaffarzoda <muzaffarzoda@gmail.com>
2023-06-04 20:30:30 -0400

    Day 01 project

--- src/main/java/com/cydeo/converter/RoleDtoConverter.java
+++ src/main/java/com/cydeo/converter/RoleDtoConverter.java
@@ -1,7 +1,9 @@
 package com.cydeo.converter;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.RoleService;
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +13,7 @@ public class RoleDtoConverter implements Converter<String, RoleDTO> {
 
     RoleService roleService;
 
-    public RoleDtoConverter(RoleService roleService) {
+    public RoleDtoConverter(@Lazy RoleService roleService) {
         this.roleService = roleService;
     }
 
