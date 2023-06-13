e5bba5566054cc8055f5fb522eab086b1c3cfdf9
Muzaffarzoda <muzaffarzoda@gmail.com>
2023-06-13 16:26:02 -0400

    Day 05 project with data

--- src/main/java/com/cydeo/repository/TaskRepository.java
+++ src/main/java/com/cydeo/repository/TaskRepository.java
@@ -18,7 +18,9 @@ public interface TaskRepository  extends JpaRepository<Task, Long> {
     @Query("SELECT COUNT(t) FROM Task t WHERE t.project.projectCode = ?1 AND t.taskStatus <> 'COMPLETE' ")
     int totalNonCompletedTasks( String codee);
 
-    @Query(value = "SELECT COUNT(*)" + "FROM tasks t JOIN projects p on t.project_id=p.id" + "WHERE p.project_code=?1 AND t.task.status='COMPLETE'", nativeQuery = true )
+    @Query(value = " SELECT COUNT(*) " +
+            " FROM tasks t JOIN projects p on t.project_id=p.id " +
+            " WHERE p.project_code=?1 AND t.task_status='COMPLETE' ", nativeQuery = true )
     int totalCompletedTasks( String projectCode);
 
     List<Task> findAllByProject(Project project);
